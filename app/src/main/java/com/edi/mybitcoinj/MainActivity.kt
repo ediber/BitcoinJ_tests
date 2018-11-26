31d43cb6b90827ebaf78ba1a0fdca16927b286e3
ediber <edibersh@gmail.com>
2018-11-26 17:19:26 -0500

    added address and ECKey

--- app/src/main/java/com/edi/mybitcoinj/MainActivity.kt
+++ app/src/main/java/com/edi/mybitcoinj/MainActivity.kt
@@ -6,6 +6,8 @@ import org.bitcoinj.core.BlockChain
 import org.bitcoinj.core.PeerGroup
 import org.bitcoinj.params.TestNet3Params
 import org.bitcoinj.wallet.Wallet
+import org.bitcoinj.store.MemoryBlockStore
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,13 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val params = TestNet3Params.get()
+        val blockStore = MemoryBlockStore(params)
+
 
         val wallet = Wallet(params)
-        val chain = BlockChain(params, wallet, null)
+        val chain = BlockChain(params, wallet, blockStore)
         val peerGroup = PeerGroup(params, chain)
         peerGroup.addWallet(wallet)
 //        peerGroup.startAndWait()
         peerGroup.start()
+
+        //obtain keys and addresses from the wallet with the following API calls:
+        val addressA = wallet.currentReceiveAddress()
+        val keyB = wallet.currentReceiveKey()
+        val addressC = wallet.freshReceiveAddress()
+
+        assert(keyB.toAddress(wallet.params).equals(addressA))
+        assert(!addressC.equals(addressA))
 
     }
 }
